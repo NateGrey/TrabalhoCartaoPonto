67a92d147ca5938e61eb8f16141ade5c211cf2f6
Charles <char__86@hotmail.com>
2012-11-01 09:35:57 -0200

    Teste de conexão com o bd.

---
+++ CartaoPonto/src/Funcionario.java
@@ -0,0 +1,14 @@
+public class Funcionario {
+	
+	private int codigo;
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String cargo;
+
+	public Funcionario(int codigo, String nome, String cpf, String endereco, String cargo) {
+		
+	}
+
+
+}
--- CartaoPonto/src/Teste.java
+++ CartaoPonto/src/Teste.java
@@ -10,13 +10,23 @@ public class Teste {
 
 		Connection con = DriverManager.getConnection(
 				"jdbc:postgresql://localhost:5432/CartaoPonto", "postgres", "senacrs");
+		
+		//Connection con = DriverManager.getConnection(
+				//"jdbc:postgresql://localhost:5432/CartaoPonto", "postgres", "lionheart");
 
 		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT nome FROM Funcionario");
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Funcionario");
 
 		while (rs.next()) {
+			int c = rs.getInt("codigoCartao");
 			String s = rs.getString("nome");
+			System.out.println(c);
 			System.out.println(s);
 		}
+		
+		//comandos para fechar os objetos
+		/*con.close();
+		stmt.close();
+		rs.close();*/
 	}
 }
